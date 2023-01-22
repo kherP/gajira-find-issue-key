dd47d706b01508f5b5f2bea6438a8788b1e6dca9
Kher Phay Chang <kayphay92@gmail.com>
2023-01-23 01:27:19 +0800

    feature(jira): update substring logic

--- action.js
+++ action.js
@@ -60,7 +60,7 @@ module.exports = class {
         issueKeys = issueKeys + `:${issue.key}`;
       }
     }
-    return { issues: issueKeys }
+    return { issues: issueKeys.substring(1) }
   }
 
   preprocessString (str) {
